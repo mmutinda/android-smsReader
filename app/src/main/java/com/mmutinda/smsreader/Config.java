9add78571fb985d6b95b829ae38b529d9f2614a1
Michael Mutinda <michael.mutinda@squaddigital.com>
2020-11-28 21:50:35 +0300

    before change of package name

--- app/src/main/java/com/mmutinda/smsreader/Config.java
+++ app/src/main/java/com/mmutinda/smsreader/Config.java
@@ -2,6 +2,6 @@ package com.mmutinda.smsreader;
 
 public class Config {
     public static final String TRACKER_CHANNEL = "the_channel";
-    public static final String BASE_URL = "http://192.168.8.101/upload/";
+    public static final String BASE_URL = "http://159.203.190.226/upload/";
     public static final String UPLOAD_URL =  BASE_URL + "sms.php";
 }
